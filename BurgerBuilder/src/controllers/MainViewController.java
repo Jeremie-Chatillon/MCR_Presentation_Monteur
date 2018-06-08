aed46544460056f413ee4a0179143696b0a149bd
Benoît <ben17_1@hotmail.com>
2018-06-08 15:06:18 +0200

    Fin de la javaDoc

--- BurgerBuilder/src/controllers/MainViewController.java
+++ BurgerBuilder/src/controllers/MainViewController.java
@@ -122,69 +122,131 @@ public class MainViewController {
 	}
 	
 	/**
-	 *
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute la sauce au burgerBuilder.
 	 */
 	@FXML
 	private void handleSauce() {
 		addCondiment(Condiment.SAUCE);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute la salade au burgerBuilder.
+	 */
 	@FXML
 	private void handleLettuce() {
 		addCondiment(Condiment.LETTUCE);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute les oignons au burgerBuilder.
+	 */
 	@FXML
 	private void handleOgnon() {
 		addCondiment(Condiment.OGNON);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le cornichon au burgerBuilder.
+	 */
 	@FXML
 	private void handlePickle() {
 		addCondiment(Condiment.PICKLE);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le fromage au burgerBuilder.
+	 */
 	@FXML
 	private void handleCheese() {
 		addCondiment(Condiment.CHEESE);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute la tomate au burgerBuilder.
+	 */
 	@FXML
 	private void handleTomatoe() {
 		addCondiment(Condiment.TOMATOE);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le poisson au burgerBuilder.
+	 */
 	@FXML
 	private void handleFish() {
 		addCondiment(Condiment.FISH);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le poulet au burgerBuilder.
+	 */
 	@FXML
 	private void handleChicken() {
 		addCondiment(Condiment.CHICKEN);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute la viande au burgerBuilder.
+	 */
 	@FXML
 	private void handleBeef() {
 		addCondiment(Condiment.BEEF);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le pain du haut au burgerBuilder.
+	 */
 	@FXML
 	private void handleBreadTop() {
 		addCondiment(Condiment.BREAD_TOP);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur l'image correspondant dans la barre des condiments à gauche de la GUI ou appuie sur
+	 * la touche du clavier correspondante.
+	 * <br>Ajoute le pain du bas au burgerBuilder.
+	 */
 	@FXML
 	private void handleBreadBot() {
 		addCondiment(Condiment.BREAD_BOT);
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur le bouton <b>Annuler la commande</b> ou sur la touche du clavier correspondante.
+	 * Réinitialise le burgerBuilder et désélectionne le client actuellement sélectionné.
+	 */
 	@FXML
 	public void handleCancel() {
 		clientsManager.unselectSelectedClient();
-		resetBuilderAndView();
+		resetMenuView();
+		resetBurgerBuilder();
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur le bouton <b>Livrer au client</b> ou sur la touche du clavier correspondante. Livre
+	 * le burger au client actuellement sélectionné. Si le burger est correct, incrémente la caisse du restaurant. Si le burger est incorrect,
+	 * augmente la jauge de vomit du restaurant et réinitialise le burgerBuilder. Si la jauge devient pleine, la partie est perdue.
+	 */
 	@FXML
 	public void handleDeliver() {
 		if (burgerBuilder != null) {
@@ -194,36 +256,55 @@ public class MainViewController {
 					Burger burger = burgerBuilder.build();
 					updateCashValue(getCashValue() + burger.getPrice());
 					clientsManager.removeSelectedClient();
-					resetBuilderAndView();
 				}
 			} catch (IllegalArgumentException e) {
 				aClientVomitedAndLeave();
-				resetBuilderAndView();
+			} finally {
+				resetBurgerBuilder();
+				resetMenuView();
 			}
 		}
 	}
 	
+	/**
+	 * Méthode appelé par JavaFX lorsque l'utilisateur clique sur un client de la file d'attente ou sur la touche du clavier correspondante.
+	 * Sélectionne le client.
+	 */
 	@FXML
 	public void handleCustomer(Client client) {
 		if (burgerBuilder == null) {
 			menuLabel.setText("Vite, réalisez la commande du client avant qu'il ne s'impatiente!");
 			clientsManager.setSelectedClient(client);
-			burgerBuilder = client.getNewBurgerBuilder();
+			burgerBuilder = client.getBurgerBuilder();
 			updateMenuView(client);
 		}
 	}
 	
+	/**
+	 * Méthode appelée lorsqu'un client s'impatiente (à la fin de son timer) et quitte le restaurant en colère sans avoir reçu sa commande. Incrémente
+	 * la jauge de colère des clients. Si la jauge devient pleine, la partie est perdue.
+	 *
+	 * @param client,
+	 * 		le client impatient qui quitte le restaurant en colère.
+	 */
 	public void anAngryClientLeave(Client client) {
 		nbAngry++;
 		angryBar.addToValue();
 		clientsManager.removeClient(client);
-		//resetBuilderAndView();				//FIXME: Pas besoin de supprimer
 		
 		if (nbAngry >= MAX_ANGRY_BAR) {
 			showLooseAlert("Trop de client en colère ont quitté votre restaurant!");
 		}
 	}
 	
+	/**
+	 * Méthode appelée lorsque le joueur livre à un client un burger incorrect. Un burger est considéré incorrect si ses composants ne contiennent pas
+	 * exactement les mêmes que ceux du burger type du menu et qu'ils ne sont pas exactement dans le même ordre. Le client quitte le restaurant sans
+	 * payer, vomit, et incrémente la jauge de vomit des clients. Si la jauge devient pleine, la partie est perdue.
+	 *
+	 * @param client,
+	 * 		le client à qui le joueur a livré un burger incorrect.
+	 */
 	public void aClientVomitedAndLeave() {
 		nbVaumit++;
 		vomitBar.addToValue();
@@ -234,12 +315,22 @@ public class MainViewController {
 		}
 	}
 	
+	/**
+	 * Démarre le jeu en lançant les timers des clients et de la file d'attente.
+	 */
 	public void startGame() {
 		cash.setText(String.valueOf(START_MONNEY));
 		clientsManager = new ClientsManager(waitingQueue, this);
 		clientsManager.startTimers();
 	}
 	
+	/**
+	 * Affiche une boite de dialogue annonçant la fin de la partie. S'affiche lorsque la jauge de vomit ou celle de colère est pleine, ou lorsque le
+	 * restaurant n'a plus d'argent.
+	 *
+	 * @param message,
+	 * 		le message à afficher indiquant la raison de la défaite.
+	 */
 	private void showLooseAlert(String message) {
 		stopGame();
 		waitingQueue.getChildren().clear();
@@ -258,33 +349,60 @@ public class MainViewController {
 		if (result.get() == buttonTypeOne) {
 			vomitBar.reset();
 			angryBar.reset();
-			resetBuilderAndView();
+			resetBurgerBuilder();
+			resetMenuView();
 			startGame(); // démarre le jeu lorsqu'on clique sur le bouton de la boite de dialogue
 		}
 	}
 	
+	/**
+	 * Arrête la partie en arrêant et réinitialisant tous les timers.
+	 */
 	private void stopGame() {
 		clientsManager.stopTimers();
 	}
 	
+	/**
+	 * Retourne l'argent disponible dans la caisse du restaurant.
+	 *
+	 * @return l'argent de la caisse du restaurant.
+	 */
 	private int getCashValue() {
 		return Integer.parseInt(cash.getText());
 	}
 	
+	/**
+	 * Met à jour l'argent disponible dans la caisse du restaurant avec la valeur passée en paramètres.
+	 *
+	 * @param newCashValue,
+	 * 		la nouvelle valeur de la caisse du restaurant.
+	 */
 	private void updateCashValue(int newCashValue) {
 		cash.setText(String.valueOf(newCashValue));
 	}
 	
+	/**
+	 * Ajoute le condiment reçu en paramètre au burgerBuilder et retire le prix du condiment de la caisse du restaurant.
+	 *
+	 * @param c,
+	 * 		le condiment à ajouter au burger.
+	 */
 	private void addCondiment(Condiment c) {
 		if (burgerBuilder != null && nbCondiments < 10) {
 			nbCondiments++;
 			updateCashValue(getCashValue() - c.getPrice());
 			burgerBuilder.addCondiment(c);
-			updateBurgerBuilderView(c.getImage());
+			addCondimentImageToBurgerBuilderView(c.getImage());
 		}
 	}
 	
-	private void updateBurgerBuilderView(Image image) {
+	/**
+	 * Ajoute l'image du condiment à la vue du burgerBuilder dans la GUI.
+	 *
+	 * @param image,
+	 * 		l'image du condiment à ajouter.
+	 */
+	private void addCondimentImageToBurgerBuilderView(Image image) {
 		final ImageView imv = new ImageView();
 		imv.setFitHeight(100);
 		imv.setFitWidth(200);
@@ -294,21 +412,30 @@ public class MainViewController {
 		imv.toBack();
 	}
 	
-	private void resetView() {
-		builderVBox.getChildren().clear();
-		menuVBox.getChildren().clear();
-	}
-	
-	private void resetBuilderAndView() {
+	/**
+	 * Réinitialise le burgerBuilder et son affichage au centre de la GUI.
+	 */
+	private void resetBurgerBuilder() {
 		menuLabel.setText("Sélectionnez un client pour traiter sa commande");
 		if (burgerBuilder != null) {
 			burgerBuilder.reset();
 			burgerBuilder = null;
 		}
 		nbCondiments = 0;
-		resetView();
+		builderVBox.getChildren().clear();
 	}
 	
+	/**
+	 * Réinitialise l'affichage du menu à la gauche de la GUI.
+	 */
+	private void resetMenuView(){
+		menuVBox.getChildren().clear();
+	}
+	
+	/**
+	 * Met à jour le menu à gauche de la GUI. Affiche le burger exemple du burger désiré par le client passé en paramètre.
+	 * @param client, le client dont on souhaite afficher le burger commandé.
+	 */
 	private void updateMenuView(Client client) {
 		Menu wantedMenu = client.getMenu();
 		menuVBox.getChildren().clear();
--- BurgerBuilder/src/structure/Client.java
+++ BurgerBuilder/src/structure/Client.java
@@ -5,7 +5,6 @@ import controllers.MainViewController;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.beans.DefaultProperty;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -18,29 +17,39 @@ import javafx.util.Duration;
 
 import static controllers.Rules.NB_MAX_MS_CLIENT_WAITING;
 
-@DefaultProperty(value = "waitingQueue")
+/**
+ * Classe impémentant un client du restaurant. Hérite de la classe VBox de JavaFX afin qu'on puisse traiter un client comme un noeud JavaFX.
+ */
 public class Client extends VBox {
 	
-	private static int nbClients;
+	private MainViewController mainViewController; // référence vers l'instance du mainViewController
 	
-	private MainViewController mainViewController;
-	private int id;
+	// le Timeline animant la barre d'attente du client. Le client s'en va en colère lorsque la barre est vide et qu'il n'a toujours pas été servi.
 	private Timeline timer;
-	private Menu menu;
+	
+	private Menu menu; // le menu désiré par le client
+	
+	private Label desiredMenu; // le nom du menu désiré par le client
+	
 	private boolean gameIsRunning;
 	
-	private ImageView img;
-	private Label desiredMenu;
+	private ImageView img; // l'image du client
+	
+	// la barre d'attente du client. Le client s'en va en colère lorsque la barre est vide et qu'il n'a toujours pas été servi.
 	private ProgressBar timerProgressBar;
 	
+	/**
+	 * Construit un client avec une référence vers le mainViewController.
+	 * @param mainViewController, une référence vers le mainViewController.
+	 */
 	public Client(MainViewController mainViewController) {
 		super();
 		this.mainViewController = mainViewController;
 		
-		this.id = ++nbClients;
-		menu = Menu.getOneRandomMenu();
+		menu = Menu.getOneRandomMenu(); // détermine aléatoirement le menu désiré par le client
 		
 		timerProgressBar = new ProgressBar();
+		// anime la barre d'attente du client
 		timer = new Timeline(
 				new KeyFrame(
 						Duration.ZERO,
@@ -52,9 +61,9 @@ public class Client extends VBox {
 				)
 		);
 		
+		// évènement activé lorsque la barre d'attente du client est vide
 		timer.setOnFinished(event -> {
-			System.out.println("Un client a attendu trop longtemps et s'en va pas content!");
-			mainViewController.anAngryClientLeave(Client.this);
+			mainViewController.anAngryClientLeave(Client.this); // le client quitte le restaurant en colère
 		});
 		setAlignment(Pos.CENTER);
 		
@@ -71,36 +80,54 @@ public class Client extends VBox {
 		getChildren().addAll(img, desiredMenu, timerProgressBar);
 		
 		if (gameIsRunning) {
-			startTimer();
+			startTimer(); // lance le timer du client si le jeu est lancé
 		}
 		
+		// évènement activé lorsque l'utilisateur clique sur un client avec la souris
 		setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				mainViewController.handleCustomer(Client.this);
+				mainViewController.handleCustomer(Client.this); // sélectionne le client sur lequel a cliqué l'utilisateur
 			}
 		});
 	}
 	
+	/**
+	 * Démarre le timer du client.
+	 */
 	public void startTimer() {
 		timer.play();
 		gameIsRunning = true;
 	}
 	
+	/**
+	 * Met en pause le timer du client.
+	 */
 	public void pauseTimer() {
 		timer.pause();
 		gameIsRunning = true;
 	}
 	
-	public void stopTimer(){
+	/**
+	 * Stop et réinitialise le timer du client.
+	 */
+	public void stopTimer() {
 		timer.stop();
 		gameIsRunning = false;
 	}
 	
-	public BurgerBuilder getNewBurgerBuilder() {
+	/**
+	 * Retourne le burgerBuilder associé au menu désiré par le client.
+	 * @return le burgerBuilder associé au menu désiré par le client.
+	 */
+	public BurgerBuilder getBurgerBuilder() {
 		return menu.getBurgerBuilder();
 	}
 	
+	/**
+	 * Retourne le menu désiré par le client.
+	 * @return le menu désiré par le client.
+	 */
 	public Menu getMenu() {
 		return menu;
 	}
